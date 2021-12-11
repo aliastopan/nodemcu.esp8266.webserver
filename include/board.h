0ca826658ff3009982815cf693a1814e5a750eed
einharan <taufan.augusta@protonmail.com>
2021-12-12 00:59:16 +0700

    feat: add easing function

--- include/board.h
+++ include/board.h
@@ -4,5 +4,11 @@
 #define SUPERFLUX D4
 
 bool isOn = false;
+bool isLapse = false;
+unsigned long start;
+unsigned long end;
+unsigned long lapse;
+unsigned long duration = 3000;
+float t;
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "rest.api.h"
 #include "board.h"
 
+#include <easing.h>
+
 void setup()
 {
   	Serial.begin(9600);
@@ -14,5 +16,37 @@ void setup()
 
 void loop()
 {
+	if(isOn)
+	{
+		analogWrite(SUPERFLUX, 255);
+
+		t = (float)((float)lapse - (float)start)/(((float)start+(float)duration) - (float)start);
+		Serial.print("t: ");
+		Serial.println(t);
+
+
+		if(!isLapse)
+		{
+			start = millis();
+			Serial.print("Start: ");
+			Serial.println(start);
+			isLapse = true;
+		}
+		else
+		{
+			lapse = millis();
+			if(lapse >= start + duration)
+			{
+				Serial.print("Lapse: ");
+				Serial.println(lapse);
+				Serial.print("Duration: ");
+				Serial.println(lapse - start);
+				isLapse = false;
+				isOn = false;
+			}
+		}
+	}
+	else
+		analogWrite(SUPERFLUX, 0);
 
 }
