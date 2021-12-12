042c79346ec7f58bd5773314201bb2b419514698
einharan <taufan.augusta@protonmail.com>
2021-12-12 11:32:08 +0700

    refactor: add class Board

--- include/board.h
+++ include/board.h
@@ -1,6 +1,8 @@
 #ifndef BOARD
 #define BOARD
 
+#include <easing.h>
+
 #define SUPERFLUX D4
 
 bool isOn = false;
@@ -10,5 +12,60 @@ unsigned long end;
 unsigned long lapse;
 unsigned long duration = 3000;
 float t;
+
+
+class Board
+{
+    public:
+        static void Setup()
+        {
+            Serial.begin(9600);
+  	        while (!Serial);
+        }
+
+        static void Loop()
+        {
+            if(isOn)
+            {
+
+                t = (float)((float)lapse - (float)start)/(((float)start+(float)duration) - (float)start);
+                easingFunction easingFunction = getEasingFunction(EaseInCirc);
+                float progress = easingFunction(t);
+                Serial.print("PWM: ");
+                Serial.print(255 * progress);
+                Serial.print(" : ");
+                Serial.print(progress);
+                Serial.println("%: ");
+
+                analogWrite(SUPERFLUX, 255 * progress);
+
+                if(!isLapse)
+                {
+                    start = millis();
+                    Serial.print("Start: ");
+                    Serial.println(start);
+                    isLapse = true;
+                }
+                else
+                {
+                    lapse = millis();
+                    if(lapse >= start + duration)
+                    {
+                        Serial.print("Lapse: ");
+                        Serial.println(lapse);
+                        Serial.print("Duration: ");
+                        Serial.println(lapse - start);
+                        isLapse = false;
+                        isOn = false;
+                        t = 0.00;
+                    }
+                }
+            }
+            else
+                analogWrite(SUPERFLUX, 0);
+        }
+};
+
+
 
 #endif
